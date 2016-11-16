e7ada5734c0495f1afb32c52b45679224e577b94
JUSTIN-PC\Justin <timentelechy@gmail.com>
2016-11-16 00:56:16 -0500

    Here's my progress on the second element.

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,23 @@
 /**
  * Created by Justin on 10/24/2016.
  */
+if (document.getElementById('radWin').check == true){
+    window.location.href='EndPage.html'
+}
+
+function SubmitAnswers() {
+    var parent = document.getElementById("element-test");
+    var newElement = document.createElement('input');
+    newElement.setAttribute('type', 'radio');
+    parent.appendChild(newElement);
+}
+
+function replaceViaElements() {
+    var parent = document.getElementById("element-test");
+
+    while (parent.hasChildNodes()) {
+        parent.removeChild(parent.lastChild);
+    }
 
 
+}
